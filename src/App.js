1793513272773dec63c4641dfa35883015f08fb5
Efren Serrano <omar.serrano90@gmail.com>
2020-11-16 19:52:08 -0600

    changes for v4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola Chiquinovia</h1>
+        <h1>Hola Panzon... no es mucho pero es trabajo honesto</h1>
       </header>
     </div>
   );
